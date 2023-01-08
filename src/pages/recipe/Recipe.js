8bb7b23ac8193d55d41ca1b0d74449592fc3195d
Andrew Cho <cisdell@gmail.com>
2023-01-08 00:14:05 -0500

    added recipe components

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,10 +1,29 @@
-import './Recipe.css';
-import React from 'react'
+import "./Recipe.css";
+import React from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 
-function Recipe() {
+export default function Recipe() {
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data: recipe, isPending, error } = useFetch(url);
+
   return (
-    <div>Recipe</div>
-  )
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
-
-export default Recipe

657bf525ee420a52377c49d418ead697d6bbc9c7
Andrew Cho <cisdell@gmail.com>
2023-01-07 23:10:57 -0500

    made seperate components for recipes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,13 @@
-import './Home.css'
-import useFetch from '../../hooks/useFetch'
-import React from 'react'
+import "./Home.css";
+import RecipeList from '../../components/RecipeList'
+import { useFetch } from "../../hooks/useFetch";
+import React from "react";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
+  return (<div className="home">
+    {error && <p className="error">{error}</p>}
+    {isPending && <p className="loading">Loading..</p>}
+    {data && <RecipeList recipes={data}/>}
+    </div>);
 }

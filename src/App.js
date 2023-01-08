42f87e2c0d4007367ac76945ecf50f54fe105832
Andrew Cho <cisdell@gmail.com>
2023-01-07 21:37:37 -0500

    added some css

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import { BrowserRouter, Routes, Route, navLink } from "react-router-dom";
 //Styles
 import "./App.css";
 //components
-import Home from './pages/home/Home'
-import Create from './pages/create/Create'
-import Recipe from './pages/recipe/Recipe'
-import Search from './pages/search/Search'
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/create" element={<Create/>} />
-      <Route path="/recipes/:id" element={<Recipe/>} />
-      <Route path="/search" element={<Search/>} />
-      <Route path="*" element={<Home />} />
-    </Routes>
-    </BrowserRouter>
-
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/recipes/:id" element={<Recipe />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

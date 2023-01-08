e5510303679ff8c7c11971c06c98912c3496fc8f
Andrew Cho <cisdell@gmail.com>
2023-01-07 20:48:39 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import { BrowserRouter, Routes, Route, navLink } from "react-router-dom";
+
+//Styles
+import "./App.css";
+//components
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
 
 function App() {
-  return (
-    <div className="App">
+  return <div className="App">
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/create" element={<Create/>} />
+      <Route path="/recipes/:id" element={<Recipe/>} />
+      <Route path="/search" element={<Search/>} />
+      <Route path="*" element={<Home />}
+    </Routes>
+    </BrowserRouter>
 
-    </div>
-  );
+  </div>;
 }
 
 export default App;
---
+++ src/pages/create/Create.js
@@ -0,0 +1,6 @@
+import "./Create.css";
+import React from "react";
+
+export default function Create() {
+  return <div>Create</div>;
+}

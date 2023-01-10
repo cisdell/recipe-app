2681eb9019a7a23d4a791acb3b9c187e54cc9c7b
Andrew Cho <cisdell@gmail.com>
2023-01-09 22:53:58 -0500

    custom hooks for useTheme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,18 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 import React from "react";
+import {useTheme} from '../hooks/useTheme'
 //styles
 import "./Navbar.css";
 import Searchbar from "./Searchbar";
 
 export default function Navbar() {
-  const {color} = useContext(ThemeContext);
+  // const { color } = useContext(ThemeContext);
   // console.log(color)
+  const {color} = useTheme();
 
   return (
-    <div className="navbar" style={{background: color}}>
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>The Thomas Keller Shop</h1>
